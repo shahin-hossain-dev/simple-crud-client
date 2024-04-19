f5a85779dce5f3e616f5cd0429197f2205bb44c7
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-19 20:40:27 +0600

    55-3 Create Client Side Post And Insert Single User Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
 
 function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="name" />
+        <br />
+        <input type="email" name="email" placeholder="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
